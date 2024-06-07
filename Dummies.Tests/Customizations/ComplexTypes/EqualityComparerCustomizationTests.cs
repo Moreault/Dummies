4d7ba98192aa6dfe566832947660d5fbb322ed94
Mathieu Moreault <seranx@gmail.com>
2024-06-07 12:50:19 -0400

    Added a customization for EqualityComparer which always returns the default

---
+++ Dummies.Tests/Customizations/ComplexTypes/EqualityComparerCustomizationTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Dummies.Tests.Customizations.ComplexTypes;
+
+[TestClass]
+public sealed class EqualityComparerOfIntCustomizationTests : EqualityComparerCustomizationTester<int>;
+
+[TestClass]
+public sealed class EqualityComparerOfStringCustomizationTests : EqualityComparerCustomizationTester<string>;
+
+public abstract class EqualityComparerCustomizationTester<T> : Tester
+{
+    [TestMethod]
+    public void Equals_WhenComparingWithSameValues_ShouldBeTrue()
+    {
+        //Arrange
+        var comparer = Dummy.Create<EqualityComparer<T>>();
+        var value1 = Dummy.Create<T>();
+        var value2 = value1;
+
+        //Act
+        var result = comparer.Equals(value1, value2);
+
+        //Assert
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Equals_WhenComparingWithSameValuesUsingInterface_ShouldBeTrue()
+    {
+        //Arrange
+        var comparer = Dummy.Create<IEqualityComparer<T>>();
+        var value1 = Dummy.Create<T>();
+        var value2 = value1;
+
+        //Act
+        var result = comparer.Equals(value1, value2);
+
+        //Assert
+        result.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void Equals_WhenComparingWithDifferentValues_ShouldBeFalse()
+    {
+        //Arrange
+        var comparer = Dummy.Create<EqualityComparer<T>>();
+        var value1 = Dummy.Create<T>();
+        var value2 = Dummy.Create<T>();
+
+        //Act
+        var result = comparer.Equals(value1, value2);
+
+        //Assert
+        result.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public void Equals_WhenComparingWithDifferentValuesUsingInterface_ShouldBeFalse()
+    {
+        //Arrange
+        var comparer = Dummy.Create<IEqualityComparer<T>>();
+        var value1 = Dummy.Create<T>();
+        var value2 = Dummy.Create<T>();
+
+        //Act
+        var result = comparer.Equals(value1, value2);
+
+        //Assert
+        result.Should().BeFalse();
+    }
+}
---
+++ Dummies/Customizations/ComplexTypes/EqualityComparerCustomization.cs
@@ -0,0 +1,14 @@
+﻿namespace ToolBX.Dummies.Customizations.ComplexTypes;
+
+[AutoCustomization]
+public sealed class EqualityComparerCustomization : CustomizationBase
+{
+    protected override IEnumerable<Type> Types { get; } = [typeof(EqualityComparer<>), typeof(IEqualityComparer<>)];
+
+    protected override IDummyBuilder BuildMe(IDummy dummy, Type type) => dummy.Build<object>().FromFactory(() =>
+    {
+        var generic = type.GetGenericArguments()[0];
+        var defaultComparerProperty = typeof(EqualityComparer<>).MakeGenericType(generic).GetSingleProperty(nameof(EqualityComparer<int>.Default));
+        return defaultComparerProperty.GetValue(null)!;
+    });
+}
